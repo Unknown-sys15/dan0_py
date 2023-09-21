cdf6eda6ac2b19e7fbb609bc75506a990b5dfa6f
Unknown-sys15 <janrakar55@gmail.com>
2023-09-21 11:35:36 +0200

    osnovni progr

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def main():
-    pass
+#! /usr/bin/python2
+#define interpreter -^
+#which python2 -> terminalski ukaz
+
+
+#ch mod -> change indetifier -> spremenis dovoljenje da lahko runas progr
+# r   w   x
+# 4   2   1
+#Za izvajanje pozeni
+#chmod +x main.py
+#./main.py
+
+import time
+
+def main():#z main funkcijo omejim scope
+    print "testic" #primer da tudi to dela ceprav podcrta
+    time.sleep(0.1)
+    print("Moje stevilo: %d" % 2)
 
 if __name__== "__main__": #enviroment var
     main()
